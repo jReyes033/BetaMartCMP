df3c7db470ee072c0105fc82481dad7f4b2db796
--\Nuela Rabino <rabinonuelajane38@gmail.com>
2024-05-31 00:18:16 +0800

    "Added functionalities for View, Update, Delete using Modal and alert message"

--- frontend_vue/src/components/ProductList.vue
+++ frontend_vue/src/components/ProductList.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="ProductList">
     <h2>Product List</h2>
-    <button class="btn btn-primary add-product-button" @click="addUser">Add Product</button>
     <div class="table-responsive">
       <table class="table table-striped table-bordered">
         <thead class="thead-dark">
@@ -28,118 +27,133 @@
             <td>
               <button class="btn btn-info btn-sm" @click="viewProduct(product)">View</button>
               <button class="btn btn-warning btn-sm" @click="editProduct(product)">Update</button>
-              <button class="btn btn-danger btn-sm" @click="deleteProduct(product)">Delete</button>
+              <button class="btn btn-danger btn-sm" @click="confirmDelete(product)">Delete</button>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="editModalLabel">Edit Product Details</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
+    <Modal v-if="showEditModal" @close="showEditModal = false">
+      <template v-slot:header>
+        <h5>Edit Product Details</h5>
+      </template>
+      <template v-slot:body>
+        <form @submit.prevent="saveProduct">
+          <div class="form-group">
+            <label for="productName">Name</label>
+            <input type="text" class="form-control" id="productName" v-model="editProductData.name">
           </div>
-          <div class="modal-body">
-            <form @submit.prevent="saveProduct">
-              <div class="form-group">
-                <label for="productName">Name</label>
-                <input type="text" class="form-control" id="productName" v-model="editProductData.name">
-              </div>
-              <div class="form-group">
-                <label for="productDescription">Description</label>
-                <input type="text" class="form-control" id="productDescription" v-model="editProductData.description">
-              </div>
-              <div class="form-group">
-                <label for="productPrice">Price</label>
-                <input type="number" class="form-control" id="productPrice" v-model="editProductData.price">
-              </div>
-              <button type="submit" class="btn btn-primary">Save</button>
-            </form>
+          <div class="form-group">
+            <label for="productDescription">Description</label>
+            <input type="text" class="form-control" id="productDescription" v-model="editProductData.description">
           </div>
-        </div>
-      </div>
-    </div>
+          <div class="form-group">
+            <label for="productPrice">Price</label>
+            <input type="number" class="form-control" id="productPrice" v-model="editProductData.price">
+          </div>
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+      </template>
+    </Modal>
+
+    <ViewModal v-if="showViewModal" :product="viewProductData" @close="showViewModal = false" />
+
   </div>
 </template>
 
 <script>
-import $ from 'jquery';
+import Modal from './EditModal.vue';
+import ViewModal from './ViewModal.vue';
 
 export default {
-    name: 'ProductList',
-    data() {
-        return {
-            products: [],
-            editProductData: {
-                id: null,
-                name: '',
-                description: '',
-                price: null
-            }
-        };
+  name: 'ProductList',
+  components: {
+    Modal,
+    ViewModal
+  },
+  data() {
+    return {
+      products: [],
+      showEditModal: false,
+      showViewModal: false,
+      editProductData: {
+        id: null,
+        name: '',
+        description: '',
+        price: null
+      },
+      viewProductData: {
+        id: null,
+        name: '',
+        description: '',
+        price: null,
+        quantity: null
+      }
+    };
+  },
+  mounted() {
+    this.fetchProducts();
+  },
+  methods: {
+    fetchProducts() {
+      fetch('http://127.0.0.1:8000/api/products', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          this.products = data;
+        })
+        .catch(error => {
+          console.error('Error fetching products:', error);
+        });
     },
-    mounted() {
-        this.fetchProducts();
+    viewProduct(product) {
+      this.viewProductData = { ...product };
+      this.showViewModal = true;
     },
-    methods: {
-        fetchProducts() {
-            fetch('http://127.0.0.1:8000/api/products', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    this.products = data;
-                })
-                .catch(error => {
-                    console.error('Error fetching products:', error);
-                });
-        },
-        viewProduct(product) {
-            console.log('View product', product);
-        },
-        editProduct(product) {
-            this.editProductData = { ...product };
-            $('#editModal').modal('show');
-        },
-        saveProduct() {
-            const index = this.products.products.findIndex(p => p.id === this.editProductData.id);
-            if (index !== -1) {
-                this.$set(this.products.products, index, { ...this.editProductData });
-            }
-            $('#editModal').modal('hide');
-        },
-        deleteProduct(product) {
-            console.log('Delete product', product);
-        },
-        increaseQuantity(product) {
-            product.quantity++;
-        },
-        decreaseQuantity(product) {
-            if (product.quantity > 0) {
-                product.quantity--;
-            }
-        },
-        addUser() {
-            console.log('Add user clicked');
-        }
+    editProduct(product) {
+      this.editProductData = { ...product };
+      this.showEditModal = true;
+    },
+    saveProduct() {
+      const index = this.products.products.findIndex(p => p.id === this.editProductData.id);
+      if (index !== -1) {
+        this.products.products.splice(index, 1, { ...this.editProductData });
+      }
+      this.showEditModal = false;
+    },
+    confirmDelete(product) {
+      const confirmMessage = `Are you sure you want to delete product id: ${product.id} product name: ${product.name} product description: ${product.description} price: ${product.price}?`;
+      if (window.confirm(confirmMessage)) {
+        this.deleteProduct(product);
+      }
+    },
+    deleteProduct(product) {
+      const index = this.products.products.findIndex(p => p.id === product.id);
+      if (index !== -1) {
+        this.products.products.splice(index, 1);
+      }
+    },
+    increaseQuantity(product) {
+      product.quantity++;
+    },
+    decreaseQuantity(product) {
+      if (product.quantity > 0) {
+        product.quantity--;
+      }
     }
+  }
 }
 </script>
-
 
 <style>
 .ProductList {
@@ -173,6 +187,31 @@ export default {
 .btn-info {
   color: white;
   background-color: #17a2b8;
+  border-color: #17a2b8;
 }
 
+.btn-warning {
+  color: white;
+  background-color: #ffc107;
+  border-color: #ffc107;
+}
+
+.btn-danger {
+  color: white;
+  background-color: #dc3545;
+  border-color: #dc3545;
+}
+
+.btn-secondary {
+  color: white;
+  background-color: #6c757d;
+  border-color: #6c757d;
+}
+
+.btn-sm {
+  padding: 0.25rem 0.5rem;
+  font-size: 0.875rem;
+  line-height: 1.5;
+  border-radius: 0.2rem;
+}
 </style>
