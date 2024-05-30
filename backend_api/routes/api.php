070ffc6257e942f5606d48426a18e4895384ce6f
Ivan <151478162+ranadaivan@users.noreply.github.com>
2024-05-30 23:04:44 +0800

    API: user account

--- backend_api/routes/api.php
+++ backend_api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
@@ -19,9 +20,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// API Routes for useraccount CRUD Operations
+
+Route::get('account', [AccountController::class, 'index']);
+Route::post('account', [AccountController::class,'store']);
+Route::get('account/{id}', [AccountController::class,'show']);
+Route::get('account/{id}/edit', [AccountController::class,'edit']);
+Route::put('account/{id}/edit', [AccountController::class,'update']);
+Route::delete('account/{id}/delete', [AccountController::class,'destroy']);
+
+// API Routes for product CRUD operations
+
 Route::get('products', [ProductController::class,'index']);
 Route::post('products', [ProductController::class,'store']);
 Route::get('products/{id}', [ProductController::class,'show']);
 Route::get('products/{id}/edit', [ProductController::class,'edit']);
 Route::put('products/{id}/edit', [ProductController::class,'update']);
 Route::delete('products/{id}/delete', [ProductController::class,'destroy']);
+
+
--- frontend_vue/src/router/index.js
+++ frontend_vue/src/router/index.js
@@ -1,22 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import ProductView from '@/views/ProductView.vue'
+import UserView from '@/views/UserView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
   {
     path: '/products',
     name: 'Product List',
     component: ProductView
+  },
+  {
+    path: '/user',
+    name: 'User List',
+    component: UserView
   }
 ]
 
